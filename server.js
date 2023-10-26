af2a4dacc7a40d246195ede33c8eeffb73404e07
AshutoshRaghubansi <148970978+AshutoshRaghubansi@users.noreply.github.com>
2023-10-26 15:44:32 +0530

    Update server.js
    yes

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ glob.sync("./models/*.js").forEach(function (file) {
 
 // Start our app!
 const app = require("./app");
-app.set("port", process.env.PORT || 80);
+app.set("port", process.env.PORT || 5050);
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running → On PORT : ${server.address().port}`);
 });
